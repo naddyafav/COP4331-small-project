24e71efcf3ed0c024474457f0a1d9a87f8198dfa
AlexSong97 <alexsongprogrammer@gmail.com>
2026-04-09 20:22:43 -0400

    Refactor UpdateContact.php for better readability

--- html/API/UpdateContact.php
+++ html/API/UpdateContact.php
@@ -1,52 +1,66 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-	$phone = $inData["phone"];
-	$email = $inData["email"];
-	$userId = $inData["userId"];
-    $contactId = $inData["contactId"];
+$inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "AllAccess", "SmallProject1", "contact_manager");
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$phone = $inData["phone"];
+$email = $inData["email"];
+$userId = $inData["userId"];
+$contactId = $inData["contactId"];
 
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare(
-            "UPDATE contacts SET FirstName=?, LastName=?, Email=?, Phone=? WHERE ContactID=? AND UserID=?");
-		$stmt->bind_param("ssssii", $firstName, $lastName, $email, $phone, $contactId, $userId);
-		$stmt->execute();
+$conn = new mysqli("localhost", "AllAccess", "SmallProject1", "contact_manager");
 
-		if ($stmt->affected_rows > 0) {
-    		returnWithError("");
-		} else {
-    		returnWithError("Failed to update contact.");
-		}
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    $stmt = $conn->prepare(
+        "UPDATE contacts
+         SET FirstName=?, LastName=?, Email=?, Phone=?
+         WHERE ContactID=? AND UserID=?"
+    );
 
-		$stmt->close();
-		$conn->close();
-	}
+    $stmt->bind_param("ssssii", $firstName, $lastName, $email, $phone, $contactId, $userId);
+    $stmt->execute();
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+    if ($stmt->affected_rows >= 0)
+    {
+        returnWithSuccess();
+    }
+    else
+    {
+        returnWithError("Failed to update contact.");
+    }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		sendResultInfoAsJson(json_encode([
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    sendResultInfoAsJson(json_encode([
         "error" => $err
     ]));
-	}
-	
+}
+
+function returnWithSuccess()
+{
+    sendResultInfoAsJson(json_encode([
+        "error" => ""
+    ]));
+}
+
 ?>
